ab1ab77fe258f112b79411cf136ed934743fad39
solrak <luiscarlosquesada@gmail.com>
2024-12-06 00:33:00 -0600

    full solution for 12/03/24

--- src/03.py
+++ src/03.py
@@ -5,4 +5,22 @@ with open("data/03") as file:
 
     data = file.read()
 
-print(sum([math.prod(list(map(int, re.findall(r"\b\d{1,3}\b", x)))) for x in re.findall(r'mul\(\b\d{1,3}\b,\b\d{1,3}\b\)', data)]))
+is_active = True
+cum = 0
+matches = [x for x in re.findall(r"(mul\(\b\d{1,3}\b,\b\d{1,3}\b\)|do\(\)|don't\(\))", data)]
+
+for match in matches:
+    print(match)
+    if 'mul' in match:
+        if is_active:
+            cum += math.prod(list(map(int, re.findall(r"\b\d{1,3}\b", match))))
+
+    elif "don't" in match:
+        is_active = False
+
+    elif 'do' in match:
+        is_active = True
+    
+
+print(cum)
+
