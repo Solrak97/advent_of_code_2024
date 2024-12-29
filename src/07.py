16db75b6b34cd471031aaa3d60d272d13630cce2
solrak <luiscarlosquesada@gmail.com>
2024-12-29 17:05:53 -0600

    well, its clean code but thats fucked up lol

---
+++ src/07.py
@@ -0,0 +1,46 @@
+import itertools
+
+equations = []
+with open('./data/07') as file:
+    for row in file:
+        nums = row.split(' ')
+        res = int(nums[0][:-1])
+        operands = list(map(int, nums[1:]))
+
+        equations.append(
+            {
+            'result': res,
+            'operands': operands    
+            })
+            
+
+def solve(operands, operators):
+    queue = []
+    queue.append(operands[0])
+
+    for i in range(1, len(operands)):
+
+        a = queue.pop()
+        b = operands[i]
+
+        queue.append(eval(f'{a}{operators[i-1]}{b}'))
+
+    return queue[0]
+
+
+# Solve for the sum of valid equations
+cum = 0
+
+for equation in equations:
+    res = equation['result']
+    oper = equation['operands']
+
+    operand_combinations = list(itertools.product(['+', '*'], repeat=(len(oper) - 1)))
+    
+    for combination in operand_combinations:
+
+        if res == solve(oper, combination):
+            cum += res
+            break
+
+print(cum)
